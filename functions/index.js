c7955c6a72f7366b91c89fabeec9b7d3166deea7
salma <salma.17m@gmail.com>
2019-12-29 03:00:02 +0100

    Get specified question and validate answer + handle response and exceptions data and messages

--- functions/index.js
+++ functions/index.js
@@ -3,53 +3,84 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
+//Handle Cross-origin resource sharing
+const cors = require('cors')({origin: true});
+
 admin.initializeApp({credential: admin.credential.applicationDefault()});
 db = admin.firestore();
 
 /*
-  function returns list of questions
+  Function returns list of questions
 */
 exports.getQuestionsList = functions.https.onRequest((request, response) => {
-    let results = [];
-    db.collection('questions').get()
-        .then(questionsSnapchot => {questionsSnapchot.forEach(doc =>  {
-             let question = doc.data();
-             question.id = doc.id;
-             results.push(question);
-          });
-
-        response.send(responseDataSuccess(results));
+    return cors(request, response, () => {
+        let results = [];
+        db.collection('questions').get().then(questionsSnapchot => {
+            questionsSnapchot.forEach(doc => {
+                let question = doc.data();
+                question.id = doc.id;
+                results.push(question);
+            });
+            response.send(responseDataSuccess(results));
         }).catch(err => {
             response.send(responseDataFailure());
         });
+    });
 });
 
 /*
-  function validates the answer of a specific question
+  Function returns a specific question by id
+*/
+exports.getQuestion = functions.https.onRequest((request, response) => {
+    return cors(request, response, () => {
+        let questionId = request.query.id;
+
+            db.collection('questions').doc(questionId).get().then(questionSnapshot => {
+            if (!questionSnapshot.exists) {
+                response.send(responseDataFailure("Question not found"));
+            } else {
+                let question = questionSnapshot.data();
+                let responseData = [{
+                    content: question.content,
+                    theme: question.theme,
+                    id: questionSnapshot.id
+                }];
+                response.send(responseDataSuccess(responseData));
+            }
+        }).catch(err => {
+            response.send(responseDataFailure(err, 400));
+        });
+    });
+});
+
+/*
+  Function validates the answer of a specific question
 */
 exports.validateAnswer = functions.https.onRequest((request, response) => {
-    let questionId = request.query.id;
-    let answer = request.query.answer;
+    return cors(request, response, () => {
+        let questionId = request.query.id;
+        let answer = request.query.answer;
+        let answerMessage = "";
 
-    db.collection('questions').doc(questionId).get()
-        .then(questionSnapshot => {
+        db.collection('questions').doc(questionId).get().then(questionSnapshot => {
             if (!questionSnapshot.exists) {
-                throw new Error("Question not found");
+                response.send(responseDataFailure("Question not found"));
             } else {
-                if (questionSnapshot.get('rightAnswer') === answer) {
-                    response.send(responseDataSuccess());
+                if (eval(questionSnapshot.get('rightAnswer')) === eval(answer)) {
+                    answerMessage = "Bravo! C'est la bonne réponse.";
                 } else {
-                    throw new Error("It is not the right answer!");
+                    answerMessage = "ZUT!  Échoué.";
                 }
+                response.send(responseDataSuccess([answerMessage]));
             }
-        })
-        .catch(err => {
-            response.send(responseDataFailure(400, err));
+        }).catch(err => {
+            response.send(responseDataFailure(err, 400));
         });
+    });
 });
 
 /*
- data response returned successfully
+  Data response returned successfully
 */
 function responseDataSuccess(data = []) {
     return {
@@ -62,9 +93,9 @@ function responseDataSuccess(data = []) {
 }
 
 /*
- data response returned with failure
+  Data response returned with failure
 */
-function responseDataFailure(statusCode = 404, error = "Data not found") {
+function responseDataFailure(error = "Data not found", statusCode = 404) {
     return {
         'result': 'nOK',
         'statusCode': statusCode,
